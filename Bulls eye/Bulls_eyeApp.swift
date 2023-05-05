fc1237eff36de19607469d27166a2eb2c6a3f03d
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2023-05-05 12:56:05 +0300

    Initial Commit

---
+++ Bulls eye/Bulls_eyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_eyeApp.swift
+//  Bulls eye
+//
+//  Created by Abdulaziz Yesuf Asmare on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_eyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
